46917cbd8fd545c14d105cf00e5afcd8cfa8287d
Brad Miller <bfm@bradfordmiller.org>
2012-11-03 23:55:34 -0400

    Got rid of getters/setters annotations, overkill for what I need.

--- tagparser.py
+++ tagparser.py
@@ -1,6 +1,6 @@
 import abc
 from entryrecord import EntryRecord
-from globals import *
+from globals import LOG_NAME, LINKEDIN_URL
 from parserhelper import ParserHelper
 import logging
 
@@ -9,43 +9,23 @@ class TagParser(object):
     __metaclass__ = abc.ABCMeta
     
     def __init__(self, html):
-        self._html = html
-        self._entry_record = EntryRecord()
-        self._logger = logging.getLogger(LOG_NAME) 
+        self.html = html
+        self.entry_record = EntryRecord()
+        self.logger = logging.getLogger(LOG_NAME) 
+        self.id = 0
 
     @abc.abstractmethod
     #protected abstract method: Parses the html in the tag
-    def _parse_tag(self, input):
+    def _parse_tag(self, html_tag):
         return
 
     #Enumeration for different parsed html tags
     (Anon, Semiknown, Known) = range(1,4)
-    
-    @property
-    def html(self):
-        return self._html
-    
-    @property
-    def entry_record(self):
-        return self._entry_record
-
-    @entry_record.setter
-    def entry_record(self, value):
-        self._entry_record = value
-    
-    @property
-    def logger(self):
-        return self._logger
-
-    @logger.setter
-    def logger(self, value):
-        self._entry_record = value
 
     #Persists the parsed html data to the database
-    def save_tag_contents(self):
+    def save(self):
         self._entry_record = self._parse_tag(self)
-        entry_id = self._entry_record.save()
-        return entry_id        
+        self.id = self.entry_record.save()       
 
     def __repr__(self):
         return "[TagParser: EntryRecord={0}]".format(self._entry_record)
@@ -56,31 +36,24 @@ class AnonTagParser(TagParser):
     def __init__(self, html):
         TagParser.__init__(self, html)
     
-    #Parses the tag
-    def _parse_tag(TagParser, self):
+    def _parse_tag(self, TagParser):
         
-        try:
-
-            TagParser.entry_record.entry_type_id = TagParser.Anon
-            TagParser.entry_record.entry_name = 'Anonymous LinkedIn User'
-            TagParser.entry_record.profile_url = ''
-            TagParser.entry_record.entry_job_title = ''
-            TagParser.entry_record.entry_location = ''
-            TagParser.entry_record.entry_region = ''
-            TagParser.entry_record.semi_known_main_entry_id = 0
-
-            return TagParser.entry_record
-       
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+        TagParser.entry_record.entry_type_id = TagParser.Anon
+        TagParser.entry_record.entry_name = 'Anonymous LinkedIn User'
+        TagParser.entry_record.profile_url = ''
+        TagParser.entry_record.entry_job_title = ''
+        TagParser.entry_record.entry_location = ''
+        TagParser.entry_record.entry_region = ''
+        TagParser.entry_record.semi_known_main_entry_id = 0
+    
+        return TagParser.entry_record
 
 #This class handles parsing logic for semi-known contacts
 class SemiKnownTagParser(TagParser):
 
     def __init__(self, html):
         TagParser.__init__(self, html)
-        self.SEMI_REGEX_PATTERN = "<a href='/wvmx/profile/redherring?[^>]*>.*</a>"
+        self.REGEX = "<a href='/wvmx/profile/redherring?[^>]*>.*</a>"
 
     @property
     def main_entry_id(TagParser):
@@ -90,171 +63,120 @@ class SemiKnownTagParser(TagParser):
     def semi_known_url(TagParser):
         return TagParser.entry_record.profile_url
 
-    #Parses the tag
-    def _parse_tag(TagParser, self):
+    def _parse_tag(self, TagParser):
 
-        try:
+        TagParser.entry_record.entry_type_id = TagParser.Semiknown
+        TagParser.entry_record.entry_name = self.get_description(TagParser)
+        TagParser.entry_record.profile_url = self.get_url(TagParser)
+        TagParser.entry_record.entry_job_title = ''
+        TagParser.entry_record.entry_location = ''
+        TagParser.entry_record.entry_region = ''
+        TagParser.entry_record.semi_known_main_entry_id = 0
 
-            TagParser.entry_record.entry_type_id = TagParser.Semiknown
-            TagParser.entry_record.entry_name = self.get_semi_known_description(TagParser)
-            TagParser.entry_record.profile_url = self.get_semi_known_url(TagParser)
-            TagParser.entry_record.entry_job_title = ''
-            TagParser.entry_record.entry_location = ''
-            TagParser.entry_record.entry_region = ''
-            TagParser.entry_record.semi_known_main_entry_id = 0
-    
-            return TagParser.entry_record
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+        return TagParser.entry_record
 
     #Parses the semi-known description from a contact in the html list
-    def get_semi_known_description(self, TagParser): 
-        
-        try:
-            
-            tag = ParserHelper.regex_return_first_match(TagParser.html, self.SEMI_REGEX_PATTERN)
-            return ParserHelper.get_text_between_tags(tag)
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+    def get_description(self, TagParser): 
+
+        tag = ParserHelper.match_first_pattern(TagParser.html, self.REGEX)
+        return ParserHelper.extract_tag_text(tag)
 
     #Gets the semi known URL
-    def get_semi_known_url(self, TagParser):
+    def get_url(self, TagParser):
+
+        tag = ParserHelper.match_first_pattern(TagParser.html, self.REGEX)
+    
+        start_index = tag.index("href='/")
+        title_index = tag.index("trk=")
         
-        try:
-            
-            tag = ParserHelper.regex_return_first_match(TagParser.html, self.SEMI_REGEX_PATTERN)
-        
-            start_index = tag.index("href='/")
-            title_index = tag.index("trk=")
-            
-            return LINKEDIN_URL + ParserHelper.clean_data(tag[start_index + 7:title_index])
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+        #TODO:  Clean this up
+        return LINKEDIN_URL + \
+            ParserHelper.clean_data(tag[start_index + 7:title_index])
 
 #This class handles parsing logic for fully identified contacts
 class IdentTagParser(TagParser):
 
-    def __init__(self, html, semi_known_id):
+    def __init__(self, html):
         TagParser.__init__(self, html)
 
-        if semi_known_id == None:
-            self.semi_known_id = 0
-        else:
-            self.semi_known_id = semi_known_id
+        self.semi_known_id = 0
 
     #Parses the tag
-    def _parse_tag(TagParser, self):
+    def _parse_tag(self, TagParser):
         
-        try:
+        TagParser.entry_record.entry_type_id = TagParser.Known
+        TagParser.entry_record.entry_name = self.name(TagParser)
+        TagParser.entry_record.profile_url = self.profile_url(TagParser)
+        TagParser.entry_record.job_title = self.job_title(TagParser).replace("'", "")
+        TagParser.entry_record.location = self.location(TagParser).replace("'", "")
+        TagParser.entry_record.region = self.industry(TagParser).replace("'", "")
+        TagParser.entry_record.semi_known_main_entry_id = self.semi_known_id
+        TagParser.entry_record.shares_groups = self.shares_groups(TagParser)
+        TagParser.entry_record.shares_connections = self.shares_connections(TagParser)
 
-            TagParser.entry_record.entry_type_id = TagParser.Known
-            TagParser.entry_record.entry_name = self.get_entry_name()
-            TagParser.entry_record.profile_url = self.get_profile_url()
-            TagParser.entry_record.entry_job_title = self.get_entry_job_title()
-            TagParser.entry_record.entry_location = self.get_entry_location()
-            TagParser.entry_record.entry_region = self.get_entry_industry()
-            TagParser.entry_record.semi_known_main_entry_id = self.semi_known_id
-            TagParser.entry_record.shares_groups = self.get_entry_shares_groups()
-            TagParser.entry_record.shares_connections = self.get_entry_shares_connections()
-
-            return TagParser.entry_record
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+        return TagParser.entry_record
 
     #Gets the name of the entry
-    def get_entry_name(TagParser):
-
-        try:
-
-            tag = ParserHelper.regex_return_first_match(TagParser.html, " title='View profile'>.*</a>")
-            return ParserHelper.get_text_between_tags(tag)
-
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+    def name(self, TagParser):
+        
+        pattern = " title='View profile'>.*</a>"
+        tag = ParserHelper.match_first_pattern(TagParser.html, pattern)
+        return ParserHelper.extract_tag_text(tag)
 
     #Gets the profile URL
-    def get_profile_url(TagParser):
-
-        try:
-
-            url_prefix = LINKEDIN_URL + "profile/view?id="
-            tag = ParserHelper.regex_return_first_match(TagParser.html, "<a href='/profile[^>]*>")
-
-            index_of_string = ''
-            title_string = ''
-
-            if tag.find("view?id"):
-                index_of_string = "id="
-                title_string = "&amp;authType"
-            elif tag.find("viewProfile=&amp"):
-                index_of_string = "key="
-                title_string = "&amp;authToken"
-
-            offset = len(index_of_string)
-
-            start_index = tag.index(index_of_string)
-            title_index = tag.index(title_string)
-
-            return url_prefix + ParserHelper.clean_data(tag[start_index + offset:title_index])
+    def profile_url(self, TagParser):
         
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+        pattern = "<a href=(.*)/profile[^>]*>"
+        url = "{0}profile/view?id=".format(LINKEDIN_URL)
+        tag = ParserHelper.match_first_pattern(TagParser.html, pattern)
+
+        index_of_string = ''
+        title_string = ''
+
+        if tag.find("view?id"):
+            index_of_string = "id="
+            title_string = "&amp;authType"
+        elif tag.find("viewProfile=&amp"):
+            index_of_string = "key="
+            title_string = "&amp;authToken"
+
+        offset = len(index_of_string)
+
+        start_index = offset + tag.index(index_of_string)
+        title_index = tag.index(title_string)
+        
+        return url + ParserHelper.clean_data(tag[start_index:title_index])
 
     #Gets the entry job title
-    def get_entry_job_title(TagParser):
+    def job_title(self, TagParser):
         
-        try:
+        pattern = "<dd class='title'>.*</dd>"
+        tag = ParserHelper.match_first_pattern(TagParser.html, pattern)
+        return ParserHelper.extract_tag_text(tag)
 
-            tag = ParserHelper.regex_return_first_match(TagParser.html, "<dd class='title'>.*</dd>")
-            return ParserHelper.get_text_between_tags(tag)
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
-    
     #Gets the entry location
-    def get_entry_location(TagParser):
+    def location(self, TagParser):
         
-        try:
-        
-            tag = ParserHelper.regex_return_first_match(TagParser.html, "<span class='location'>.*</span>")
-            return ParserHelper.get_text_between_tags(tag)
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
+        pattern = "<span class='location'>.*</span>"
+        tag = ParserHelper.match_first_pattern(TagParser.html, pattern)
+        return ParserHelper.extract_tag_text(tag)
 
     #Gets the entry industry
-    def get_entry_industry(TagParser):
+    def industry(self, TagParser):
         
-        try:
+        pattern = "<span class='industry'>.*</span>"
+        tag = ParserHelper.match_first_pattern(TagParser.html, pattern)
+        return ParserHelper.extract_tag_text(tag)
 
-            tag = ParserHelper.regex_return_first_match(TagParser.html, "<span class='industry'>.*</span>")
-            return ParserHelper.get_text_between_tags(tag)
-        
-        except:
-            TagParser.logger.error(sys.exc_info()[0])
-            raise
-    
     #Gets the entry shares groups
-    def get_entry_shares_groups(TagParser):
+    def shares_groups(self, TagParser):
         if TagParser.html.find("<div class='shared-groups'>") != -1:
             return 1
         else:
             return 0
 
     #Gets the entry shares connections
-    def get_entry_shares_connections(TagParser):
+    def shares_connections(self, TagParser):
         if TagParser.html.find("<div class='shared-connections'") != -1:
             return 1
         else:
@@ -263,11 +185,11 @@ class IdentTagParser(TagParser):
 class TagFactory:
     
     @staticmethod
-    def get_tag_parser(html, semi_known_id):
+    def get_tag_parser(html):
         
-        if html.find("Anonymous LinkedIn User") != -1:
+        if html.find("wvmx-anonymous-photo") != -1:
             return AnonTagParser(html)
-        elif html.find("redherring") != -1:
+        elif html.find("wvmx_profile_redherring") != -1:
             return SemiKnownTagParser(html)
         else:
-            return IdentTagParser(html, semi_known_id)
+            return IdentTagParser(html)
